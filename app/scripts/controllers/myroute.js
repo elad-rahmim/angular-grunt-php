796913ea2f8a40d297c68201c010b8f00c7cb047
elad-rahamim <elad@eready.co.il>
2016-04-27 18:15:36 +0300

    add myroute("Blog")
    add myroute("Blog")

---
+++ app/scripts/controllers/myroute.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youtubeApp.controller:MyrouteCtrl
+ * @description
+ * # MyrouteCtrl
+ * Controller of the youtubeApp
+ */
+angular.module('youtubeApp')
+.controller('MyrouteCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
+
+    $scope.frmToggle = function() {
+        $('#blogForm').slideToggle();
+    };
+
+    $http.get('http://www.ngphp.com/api/popData.php')
+
+        .success(function(data) {
+            $scope.blogs = data;
+        })
+        .error(function(err) {
+            $log.error(err);
+        });
+
+    $scope.pushData = function($params) {
+        $http.post('http://www.ngphp.com/api/pushData.php',{'title':$params.title, 'description':$params.description, 'count':$params.count })
+            .success(function(data) {
+                $scope.blogs = data;
+            })
+            .error(function(err) {
+                $log.error(err);
+            });
+    };
+
+    $scope.removeData = function($params) {
+        var cnfrm = confirm("Are you sure to delete?");
+        if(cnfrm) {
+            $http.post('http://www.ngphp.com/api/removeData.php', {'id':$params})
+                .success(function(data) {
+                    $scope.blogs = data;
+                })
+                .error(function(err) {
+                    $log.error(err);
+                });
+        } else {
+            //
+        }
+
+    };
+
+}]);
+
